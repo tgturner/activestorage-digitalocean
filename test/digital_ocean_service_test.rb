22a9f0155a4311413a490568ac4d794f6cb4a538
Graham Turner <turnertgraham@gmail.com>
2018-04-20 18:09:20 -0400

    Finally learn markdown

--- test/digital_ocean_service_test.rb
+++ test/digital_ocean_service_test.rb
@@ -10,6 +10,7 @@ class ActiveStorage::Service::DigitalOceanServiceTest < ActiveSupport::TestCase
   FIXTURE_DATA = "\211PNG\r\n\032\n\000\000\000\rIHDR\000\000\000\020\000\000\000\020\001\003\000\000\000%=m\"\000\000\000\006PLTE\000\000\000\377\377\377\245\331\237\335\000\000\0003IDATx\234c\370\377\237\341\377_\206\377\237\031\016\2603\334?\314p\1772\303\315\315\f7\215\031\356\024\203\320\275\317\f\367\201R\314\f\017\300\350\377\177\000Q\206\027(\316]\233P\000\000\000\000IEND\256B`\202".dup.force_encoding(Encoding::BINARY)
 
   setup do
+    # Need this sleep to keep from getting rate limitted
     sleep 2
     @service = ActiveStorage::Service.configure(:digital_ocean, SERVICE_CONFIGURATIONS)
     @service.upload FIXTURE_KEY, StringIO.new(FIXTURE_DATA)
